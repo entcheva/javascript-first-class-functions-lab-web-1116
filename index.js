4fa2ebab021d64b7857300be3ffb120f5ac2f583
entcheva <christina.entcheva@gmail.com>
2017-01-22 00:32:08 -0500

    Done.

---
+++ index.js
@@ -0,0 +1,24 @@
+function countdown(callback) {
+  window.setTimeout(callback, 2000)
+}
+
+function createMultiplier(num1) {
+  return function(num2){
+    return num1 * num2
+  }
+}
+
+var doubler = createMultiplier(2)
+var tripler = createMultiplier(3)
+
+
+function multiplier(num1, num2) {
+  return num1 * num2
+}
+
+var doublerWithBind = multiplier.bind(2)
+var triplerWithBind = multiplier.bind(3)
+
+
+// function createMultiplierBonus() {
+// }
