b7f9a283a9bff73cf8f2f47ea0161b091421ee31
entcheva <christina.entcheva@gmail.com>
2017-01-25 20:00:10 -0500

    Done.

--- index.js
+++ index.js
@@ -11,14 +11,9 @@ function createMultiplier(num1) {
 var doubler = createMultiplier(2)
 var tripler = createMultiplier(3)
 
-
 function multiplier(num1, num2) {
   return num1 * num2
 }
 
 var doublerWithBind = multiplier.bind(2)
 var triplerWithBind = multiplier.bind(3)
-
-
-// function createMultiplierBonus() {
-// }
